db0880c19b3d2627429d6834f3eebc3a1c002abf
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-10-21 11:15:23 +0200

    Revert "smol change"
    This reverts commit 8aacab9af6e455bc9472d45fa48a733c99dcf3cd.

--- Events/Patches/ChangingSpectatedPlayerPatch.cs
+++ Events/Patches/ChangingSpectatedPlayerPatch.cs
@@ -23,11 +23,9 @@ namespace Mistaken.Events.Patches
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
             Label continueLabel = generator.DefineLabel();
-
-            LocalBuilder player = generator.DeclareLocal(typeof(Player));
+            Label continueAndPopLabel = generator.DefineLabel();
 
             int index = newInstructions.FindIndex(x => x.opcode == OpCodes.Ret) + 1;
-            newInstructions[index].WithLabels(continueLabel);
             newInstructions.InsertRange(
                 index,
                 new CodeInstruction[]
@@ -46,9 +44,7 @@ namespace Mistaken.Events.Patches
                     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(SpectatorManager), nameof(SpectatorManager._hub))), // [ReferenceHub]
                     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Player), nameof(Player.Get), new System.Type[] { typeof(ReferenceHub) })), // [Player]
                     new CodeInstruction(OpCodes.Dup), // [Player, Player]
-                    new CodeInstruction(OpCodes.Stloc, player),
-                    new CodeInstruction(OpCodes.Brfalse_S, continueLabel), // [Player]
-                    new CodeInstruction(OpCodes.Ldloc, player),
+                    new CodeInstruction(OpCodes.Brfalse_S, continueAndPopLabel), // [Player]
                     new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]), // [this, Player]
                     new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(SpectatorManager), nameof(SpectatorManager._currentSpectatedPlayer))), // [ReferenceHub(OldSpectated), Player(Spectator)]
                     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Player), nameof(Player.Get), new System.Type[] { typeof(ReferenceHub) })), // [Player(OldSpectated), Player(Spectator)]
@@ -56,6 +52,9 @@ namespace Mistaken.Events.Patches
                     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Player), nameof(Player.Get), new System.Type[] { typeof(ReferenceHub) })), // [Player(NewSpectated), Player(OldSpectated), Player(Spectator)]
                     new CodeInstruction(OpCodes.Newobj, AccessTools.GetDeclaredConstructors(typeof(ChangingSpectatedPlayerEventArgs))[0]),  // [EventArgs]
                     new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CustomEvents), nameof(CustomEvents.InvokeChangingSpectatedPlayer))),  // []
+
+                    new CodeInstruction(OpCodes.Pop).WithLabels(continueAndPopLabel),
+                    new CodeInstruction(OpCodes.Nop).WithLabels(continueLabel),
                 });
 
             for (int z = 0; z < newInstructions.Count; z++)
