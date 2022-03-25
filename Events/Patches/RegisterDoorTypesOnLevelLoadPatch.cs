16cf07e8c18d3db419a5e920fed61674c749be4e
Xname <xnameserver@o2.pl>
2022-03-25 21:13:36 +0100

    fix rooms type unknown

--- Events/Patches/RegisterDoorTypesOnLevelLoadPatch.cs
+++ Events/Patches/RegisterDoorTypesOnLevelLoadPatch.cs
@@ -4,12 +4,20 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Collections.Generic;
+
 namespace Mistaken.Events.Patches
 {
     internal static class RegisterDoorTypesOnLevelLoadPatch
     {
         public static void Postfix()
         {
+            MEC.Timing.RunCoroutine(InvokeDelay());
+        }
+
+        private static IEnumerator<float> InvokeDelay()
+        {
+            yield return MEC.Timing.WaitForOneFrame;
             Handlers.CustomEvents.InvokeGeneratedCache();
         }
     }
